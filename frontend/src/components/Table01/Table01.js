cab9860f7e6724536e18e1b44915f876621c502b
xzhang-wework <xin.zhang4@wework.com>
2019-12-08 22:53:35 +0800

    first attemp to connect with data.

---
+++ frontend/src/components/Table01/Table01.js
@@ -0,0 +1,30 @@
+import React from "react"
+import TableRow01 from "./TableRow01"
+import wwFloors from "/Users/mynovmbr/code/django-todo-react/frontend/src/data/floor_stats.js"
+
+function Table01(props) {
+
+  const []
+
+  return (
+    <table className="card">
+      <TableRow01 
+        floorName="123"
+        floorUSF="456"
+        openDate="789"
+      />
+      <TableRow01 
+        floorName="123"
+        floorUSF="456"
+        openDate="789"
+      />
+      <TableRow01 
+        floorName="123"
+        floorUSF="456"
+        openDate="789"
+      />
+    </table>
+  )
+}
+
+export default Table01;
