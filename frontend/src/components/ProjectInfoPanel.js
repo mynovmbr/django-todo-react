aa6cce82991209e9b539f33650c6b92380ae741e
xzhang-wework <xin.zhang4@wework.com>
2019-12-08 18:09:09 +0800

    add selection function and able to show project info.

--- frontend/src/components/ProjectInfoPanel.js
+++ frontend/src/components/ProjectInfoPanel.js
@@ -1,15 +1,75 @@
-import React from "react";
-import ProjectSelector from "./ProjectSelector"
+import React, { useState }from "react";
+// import ProjectSelector from "./ProjectSelector"
+import ProjectSelectorOption from "./ProjectSelectorOption"
+import ProjectInfo from "./ProjectInfo";
+import wwBuildings from "../data/building_stats"
 
 function ProjectInfoPanel() {
+
+    const [selectedProject, setProject] = useState("select your project")
+    const [buildingInfo, setBuildingInfo] = useState({
+        buildingName: "",
+        buildingAddress: "",
+        buildingTerritory: "",
+        buildingUUID: "",
+        buildingUSF: "",
+        buildingDeskCount: "",
+        buildingRoomCount: ""
+    })
+
+    function handleSelect(event){
+        const {value, address} = event.target;
+
+        setProject(value) 
+        // setBuildingInfo(address)
+        updateBuildingInfo(value, wwBuildings)
+    }
+
+    function updateBuildingInfo(selectedBuilding, wwBuildings) {
+        wwBuildings.map(wwBuilding => {
+            if (wwBuilding.BuildingName === selectedBuilding){
+                setBuildingInfo({
+                    buildingName: wwBuilding.BuildingName,
+                    buildingAddress: wwBuilding.BuildingAddress,
+                    buildingTerritory: wwBuilding.BuildingTerritory,
+                    buildingUUID: wwBuilding.BuildingUUID,
+                    buildingUSF: wwBuilding.BuildingUSF,
+                    buildingDeskCount: wwBuilding.BuildingDeskCount,
+                    buildingRoomCount: wwBuilding.BuildingRoomCount
+                })
+                console.log(buildingInfo)
+            } 
+            // else {
+            //     setBuildingInfo("Cannot find building info.")
+            //     console.log("cannot find a match")
+            // }
+        });
+    }
+
+    function createOption(wwBuildings) {
+        return <ProjectSelectorOption 
+        key={wwBuildings.BuildingUUID}
+        name={wwBuildings.BuildingName} 
+        value={wwBuildings.BuildingName}
+        />
+    }
+
     return <div>
         <img className="card" src="./img/img_001.jpg" alt="No Image Loaded"/>
-        <p>Placeholder Text</p>
-        <ProjectSelector />
-        <p>Placeholder Text</p>
-        <p>Placeholder Text</p>
-        <p>Placeholder Text</p>
-        <p>Placeholder Text</p>
+        <p></p>
+        Select project to get start:
+          <select value={selectedProject} onChange={handleSelect}>
+            {wwBuildings.map(createOption)}
+          </select>
+        <ProjectInfo 
+            buildingName={buildingInfo.buildingName}
+            buildingAddress={buildingInfo.buildingAddress}
+            buildingTerritory={buildingInfo.buildingTerritory}
+            buildingUUID={buildingInfo.buildingUUID}
+            buildingUSF={buildingInfo.buildingUSF}
+            buildingDeskCount={buildingInfo.buildingDeskCount}
+            buildingRoomCount={buildingInfo.buildingRoomCount}
+        />
     </div>
 }
 
---
+++ frontend/src/components/ProjectSelectorOption.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ProjectSelectorOption(props) {
+
+    return <option value={props.value} >{props.name}</option>
+}
+
+export default ProjectSelectorOption
