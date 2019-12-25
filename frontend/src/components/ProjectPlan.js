9a7f7f1b7f753650dd5aee3db826cd4953dbb1fd
xzhang-wework <xin.zhang4@wework.com>
2019-12-25 11:56:07 +0800

    basic router set up done. with ProjectOverview and ProjectPlan.

--- frontend/src/components/ProjectPlan.js
+++ frontend/src/components/ProjectPlan.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 function ProjectPlan() {
-    return <div className="card">
-        <div>
-            <h2>ProjectPlan Placeholder</h2>
+    return (
+        <div className="card">
+            <div>
+                <h2>ProjectPlan Placeholder</h2>
+            </div>
+            <p>this is a ProjectPlan placeholder</p>
         </div>
-        <p>this is a ProjectPlan placeholder</p>
-    </div>    
+    )    
 }
 
 export default ProjectPlan;
